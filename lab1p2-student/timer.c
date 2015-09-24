79dc27b22ef1df96320c5a9f2419477d41306451
jmmanos <jmmanos@email.arizona.edu>
2015-09-24 05:14:14 -0700

    Added majority of code to Lab 1 Part 2.

--- lab1p2-student/timer.c
+++ lab1p2-student/timer.c
@@ -8,8 +8,20 @@
 #include <xc.h>
 #include "timer.h"
 
+initTimer2(){
+    TMR2 = 0;
+    T2CONbits.TCKPS = 0;
+    T2CONbits.TCS = 0;
+    IFS0bits.T2IF = 0;
+}
+
 void delayUs(unsigned int delay){
 
     //TODO: Create a delay for "delay" micro seconds using timer 2
-    
+    TMR2 = 0;
+    PR2 = delay*312;
+    IFS0bits.T2IF = 0;
+    T2CONbits.ON = 1;
+    while(IFS0bits.T2IF == 0);
+    T2CONbits.ON = 0;
 }
